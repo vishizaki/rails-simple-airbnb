e749860df52f7c8d6ff20416fa7dfa7d2b7de0a3
Vinicius Ishizaki <vinicius.ishizaki@gmail.com>
2018-11-07 17:02:20 -0500

    implemented css

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+flats_attributes = [
+  {
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+  },
+  {
+  name: 'Spacious Garden Flat Amsterdam',
+  address: '10 Clifton Gardens Amsterdam W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat.',
+  price_per_night: 90,
+  number_of_guests: 3
+  },
+  {
+  name: 'Light Flat Paris',
+  address: '1023 City Gardens Paris',
+  description: 'Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 60,
+  number_of_guests: 2
+  },
+  {
+  name: 'Nice flat in Las Vegas',
+  address: '1012 East Flamingo Road Las Vegas',
+  description: 'Lovely flat in downtown Vegas',
+  price_per_night: 120,
+  number_of_guests: 5
+  }
+]
+
+Flat.create!(flats_attributes)
+puts 'Finished!'
